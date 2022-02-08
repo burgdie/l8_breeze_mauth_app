f182de6591e2f14099b05d46eb7aed5fa7405b6c
Dieter <dburgstaller@gmx.de>
2022-02-08 04:20:29 +0100

    Initial commit  multi auth with breeze
    admi guard implemented and 
    admin/admin_master.blade.php file  and
    admin/index.blade.php file prepared and updated 
    panel directory in public folder copied

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function Index() {
+       return view() ;
+    }
+
+    public function Dashboard(){
+        return view('admin.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*------------ Start Admin Route         ----------- */
+Route::prefix('admin')->group(function(){
+    Route::get('/login', [AdminController::class, 'Index'])->name('login_from');
+    Route::get('/login/owner', [AdminController::class, 'Login'])->name('admin_login');
+    Route::get('/dashboard', [AdminController::class, 'Dashboard'])->name('admin.dashboard');
+
+});
+
+
+/*------------ End Admin Route         ----------- */
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
